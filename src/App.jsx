06e7c7545fb88cb1a34dc1578409ebda0c65ca74
EFDillmann <dillmanncode1999@gmail.com>
2024-02-15 00:15:26 -0300

    Shadcn añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button>Hola shad</Button>
+    </>
+  );
 }
 
 export default App;
