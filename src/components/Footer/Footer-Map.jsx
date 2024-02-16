ca54351bf79a129f83097f902ab86388c265241d
EFDillmann <dillmanncode1999@gmail.com>
2024-02-16 20:49:12 -0300

    Delete react imports

--- src/components/Footer/Footer-Map.jsx
+++ src/components/Footer/Footer-Map.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-
 const FooterMap = () => {
-	return (
-		<iframe
-			src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15864.376584350946!2d-75.5668109!3d6.2513247!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44293c5e1c61fd%3A0x24e50322eb8bad7d!2sUni%C3%B3n%20Plaza%20Centro%20Comercial!5e0!3m2!1ses-419!2sar!4v1708042188353!5m2!1ses-419!2sar"
-			width="400"
-			height="300"
-			className="relative top-0 left-0 w-full h-full"
-			loading="lazy"></iframe>
-	);
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15864.376584350946!2d-75.5668109!3d6.2513247!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44293c5e1c61fd%3A0x24e50322eb8bad7d!2sUni%C3%B3n%20Plaza%20Centro%20Comercial!5e0!3m2!1ses-419!2sar!4v1708042188353!5m2!1ses-419!2sar"
+      width="400"
+      height="300"
+      className="relative top-0 left-0 w-full h-full"
+      loading="lazy"
+    ></iframe>
+  );
 };
 
 export default FooterMap;
