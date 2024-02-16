32deeb9b76efa71246be89aa3a1d2783aa7111f8
Ares <thiagodev.martins@gmail.com>
2024-02-15 23:16:58 -0300

    Footer complete + Font

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FooterHead from "./Footer-Head";
+import FooterBody from "./Footer-Body";
+
+export const Footer = () => {
+	return (
+		<footer className="bg-gray-100 py-5">
+			<div className="container mx-auto  text-xs">
+				<FooterHead />
+				<FooterBody />
+			</div>
+		</footer>
+	);
+};
