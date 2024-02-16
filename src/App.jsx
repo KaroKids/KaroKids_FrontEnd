6ca9217eff852cdb00a57f91aff4faf64c343467
Ares <thiagodev.martins@gmail.com>
2024-02-16 02:31:25 -0300

    Font fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import "./App.css";
- 
-import { Footer } from "./components/Footer/Footer";
-import  Landing  from "./components/Home/Landing";
-import SocialBar from "./components/SocialBar";
 
+import { Footer } from "./components/Footer/Footer";
+import Landing from "./components/Home/Landing";
+import SocialBar from "./components/SocialBar";
 
 function App() {
 	return (
