c2938133eec9a12c4cfb5687cc5c703015f40e77
Ares <thiagodev.martins@gmail.com>
2024-02-16 01:46:30 -0300

    Sidebar fixed

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -1,12 +1,12 @@
 const SocialBar = () => {
 	return (
-		<nav className="flex fixed mt-24 transition-all duration-300 shadow-md bg-white">
+		<nav className="flex fixed top-20 mt-24 transition-all duration-300 shadow-md bg-white ">
 			<ul>
 				<li>
 					<a
 						href="https://www.facebook.com/YosoyKaroKids/?locale=es_LA"
 						target="_blanck"
-						className="justify-center flex h-16 text-white bg-blue-600">
+						className="justify-center flex h-12 sm:h-16 text-white bg-blue-600 rounded-tr-lg">
 						<img
 							src="/assets/social_media/facebook-color.svg"
 							className="w-12 p-2"></img>
@@ -17,7 +17,7 @@ const SocialBar = () => {
 					<a
 						href="https://www.instagram.com/yosoy.karokidsmoda/"
 						target="_blanck"
-						className="justify-center flex h-16 text-white bg-rose-500">
+						className="justify-center flex h-12 sm:h-16 text-white bg-rose-500">
 						<img
 							src="/assets/social_media/instagram-color.svg"
 							className="w-10 p-2"></img>
@@ -28,7 +28,7 @@ const SocialBar = () => {
 					<a
 						href="https://www.youtube.com/@karokidsmodainfantil4675"
 						target="_blanck"
-						className="justify-center flex h-16 text-white bg-red-600">
+						className="justify-center flex h-12 sm:h-16 text-white bg-red-600">
 						<img
 							src="/assets/social_media/youtube-color.svg"
 							className="w-12 p-2"></img>
@@ -39,7 +39,7 @@ const SocialBar = () => {
 					<a
 						href="https://wa.link/fdh8yl"
 						target="_blanck"
-						className="justify-center flex h-16 text-white bg-green-600">
+						className="justify-center flex h-12 sm:h-16 text-white bg-green-600 rounded-br-lg">
 						<img
 							src="/assets/social_media/whatsapp-color.svg"
 							className="w-10 p-2"></img>
