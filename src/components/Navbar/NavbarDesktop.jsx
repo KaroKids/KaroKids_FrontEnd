6b80d533703a28beedd219e0cf2c43fd8126c320
Ares <thiagodev.martins@gmail.com>
2024-02-16 21:52:46 -0300

    Service Detail

--- src/components/Navbar/NavbarDesktop.jsx
+++ src/components/Navbar/NavbarDesktop.jsx
@@ -1,97 +1,96 @@
 import { useEffect, useState } from "react";
 
 const NavbarDesktop = () => {
-  const [fixed, setFixed] = useState(false);
+	const [fixed, setFixed] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 10) {
-        setFixed(true);
-      } else {
-        setFixed(false);
-      }
-    };
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 10) {
+				setFixed(true);
+			} else {
+				setFixed(false);
+			}
+		};
 
-    window.addEventListener("scroll", handleScroll);
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  return (
-    <nav
-      className={`hidden h-14 w-screen bg-white px-8 md:flex items-center shadow-md shadow-gray-300 ${fixed ? "md:fixed md:z-10 md:top-0" : ""}`}
-    >
-      <ul className="flex gap-2 items-center flex-wrap font-medium md:max-w-[35%]">
-        <li>Home</li>
-        <li className="flex gap-1 items-center">
-          Recien nacido{" "}
-          <img
-            src="../../../public/assets/navbar-icons/arrow-down.svg"
-            alt="Logo de busqueda"
-            className="pt-1"
-          />
-        </li>
-        <li className="flex gap-1">
-          Bebé{" "}
-          <img
-            src="../../../public/assets/navbar-icons/arrow-down.svg"
-            alt="Logo de busqueda"
-            className="pt-1"
-          />
-        </li>
-        <li className="flex gap-1">
-          Infantil{" "}
-          <img
-            src="../../../public/assets/navbar-icons/arrow-down.svg"
-            alt="Logo de busqueda"
-            className="pt-1"
-          />
-        </li>
-        <li className="flex gap-1">
-          Junior{" "}
-          <img
-            src="../../../public/assets/navbar-icons/arrow-down.svg"
-            alt="Logo de busqueda"
-            className="pt-1"
-          />
-        </li>
-      </ul>
-      <figure className="mx-auto">
-        <img
-          src="/public/assets/images/logo-karokids.png"
-          alt="Logo de KaroKids"
-          className="w-[184px] h-[45px]"
-        />
-      </figure>
-      <ul className="flex gap-4 justify-center">
-        <li className="text-sky-500 font-medium">
-          <a href="/">Login</a> / <a href="/">Register</a>
-        </li>
-        <li>
-          <img
-            src="/public/assets/navbar-icons/search-blue.svg"
-            alt="Logo de Busqueda"
-            className="w-6 h-6"
-          />
-        </li>
-        <li>
-          <img
-            src="/public/assets/navbar-icons/cart-blue.svg"
-            alt="Logo del carrito de compras"
-            className="w-6 h-6"
-          />
-        </li>
-        <li>
-          <img
-            src="/public/assets/navbar-icons/fav-blue.svg"
-            alt="Logo de Favoritos"
-            className="w-6 h-6"
-          />
-        </li>
-      </ul>
-    </nav>
-  );
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+	return (
+		<nav
+			className={`hidden h-14 w-screen bg-white px-8 md:flex items-center shadow-md shadow-gray-300 ${fixed ? "md:fixed md:z-10 md:top-0" : ""}`}>
+			<ul className="flex gap-2 items-center flex-wrap font-medium md:max-w-[35%]">
+				<li>Home</li>
+				<li className="flex gap-1 items-center">
+					Recien nacido{" "}
+					<img
+						src="/assets/navbar-icons/arrow-down.svg"
+						alt="Logo de busqueda"
+						className="pt-1"
+					/>
+				</li>
+				<li className="flex gap-1">
+					Bebé{" "}
+					<img
+						src="/assets/navbar-icons/arrow-down.svg"
+						alt="Logo de busqueda"
+						className="pt-1"
+					/>
+				</li>
+				<li className="flex gap-1">
+					Infantil{" "}
+					<img
+						src="/assets/navbar-icons/arrow-down.svg"
+						alt="Logo de busqueda"
+						className="pt-1"
+					/>
+				</li>
+				<li className="flex gap-1">
+					Junior{" "}
+					<img
+						src="/assets/navbar-icons/arrow-down.svg"
+						alt="Logo de busqueda"
+						className="pt-1"
+					/>
+				</li>
+			</ul>
+			<figure className="mx-auto">
+				<img
+					src="/public/assets/images/logo-karokids.png"
+					alt="Logo de KaroKids"
+					className="w-[184px] h-[45px]"
+				/>
+			</figure>
+			<ul className="flex gap-4 justify-center">
+				<li className="text-sky-500 font-medium">
+					<a href="/">Login</a> / <a href="/">Register</a>
+				</li>
+				<li>
+					<img
+						src="/public/assets/navbar-icons/search-blue.svg"
+						alt="Logo de Busqueda"
+						className="w-6 h-6"
+					/>
+				</li>
+				<li>
+					<img
+						src="/public/assets/navbar-icons/cart-blue.svg"
+						alt="Logo del carrito de compras"
+						className="w-6 h-6"
+					/>
+				</li>
+				<li>
+					<img
+						src="/public/assets/navbar-icons/fav-blue.svg"
+						alt="Logo de Favoritos"
+						className="w-6 h-6"
+					/>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default NavbarDesktop;
--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -2,144 +2,143 @@ import { useState, useEffect } from "react";
 import { Button } from "../ui/button";
 import { Combobox } from "../ui/combobox";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
 import { useMediaQuery } from "@react-hook/media-query";
+import ServicesDetail from "../Services/ServicesDetail";
 
 const ProductDetail = () => {
-  const [producto, setProducto] = useState("");
-  const [cargando, setCargando] = useState(true);
-  const [error, setError] = useState(null);
-  const [selectedColor, setSelectedColor] = useState("");
-  const [isZoomed, setIsZoomed] = useState(false);
-  const isMobile = useMediaQuery("(max-width: 1024px)");
+	const [producto, setProducto] = useState("");
+	const [cargando, setCargando] = useState(true);
+	const [error, setError] = useState(null);
+	const [selectedColor, setSelectedColor] = useState("");
+	const [isZoomed, setIsZoomed] = useState(false);
+	const isMobile = useMediaQuery("(max-width: 1024px)");
 
-  const colors = [
-    { name: "Gris", value: "gray" },
-    { name: "Negro", value: "black" },
-    { name: "Rosa", value: "pink" },
-    { name: "Blanco", value: "white" },
-  ];
+	const colors = [
+		{ name: "Gris", value: "gray" },
+		{ name: "Negro", value: "black" },
+		{ name: "Rosa", value: "pink" },
+		{ name: "Blanco", value: "white" },
+	];
 
-  const handleColorChange = (color) => {
-    setSelectedColor(color);
-  };
+	const handleColorChange = (color) => {
+		setSelectedColor(color);
+	};
 
-  const handleMouseEnter = () => {
-    setIsZoomed(true);
-  };
+	const handleMouseEnter = () => {
+		setIsZoomed(true);
+	};
 
-  const handleMouseLeave = () => {
-    setIsZoomed(false);
-  };
-  useEffect(() => {
-    const productoData = {
-      nombre: "REMERA COLLARCITO NEGRO NENE",
-      precio: 6.626,
-      descripcion:
-        "Remera de jersey liviano rayado manga corta con cuello a tono. Estampa central. Composición: 100% Algodón",
-      imagen:
-        "https://acdn.mitiendanube.com/stores/867/057/products/38505-negro1-be14996f533be78dfd16715396766743-1024-1024.webp",
-    };
+	const handleMouseLeave = () => {
+		setIsZoomed(false);
+	};
+	useEffect(() => {
+		const productoData = {
+			nombre: "REMERA COLLARCITO NEGRO NENE",
+			precio: 6.626,
+			descripcion:
+				"Remera de jersey liviano rayado manga corta con cuello a tono. Estampa central. Composición: 100% Algodón",
+			imagen:
+				"https://acdn.mitiendanube.com/stores/867/057/products/38505-negro1-be14996f533be78dfd16715396766743-1024-1024.webp",
+		};
 
-    setProducto(productoData);
-    setCargando(false);
-  }, []);
+		setProducto(productoData);
+		setCargando(false);
+	}, []);
 
-  return (
-    <div className="min-h-screen py-0 px-4 text-center">
-      <div className="px-2  text-center xl:grid xl:grid-cols-1 xl:h-[750px]">
-        <div
-          className={
-            !isMobile
-              ? "flex xl:flex-row xl:gap-6  xl:items-center lg:mx-20 "
-              : " flex-center"
-          }
-        >
-          <div className="px-10 ">
-            <Carousel
-              orientation="horizontal"
-              className="xl:w-[600px] xl:shadow-lg"
-            >
-              <CarouselContent>
-                {Array.from({ length: 3 }).map((_, index) => (
-                  <CarouselItem key={index}>
-                    <div>
-                      <Card>
-                        <CardContent className="p-6">
-                          <img
-                            src={producto.imagen}
-                            alt={producto.nombre}
-                            onMouseEnter={handleMouseEnter}
-                            onMouseLeave={handleMouseLeave}
-                            className={`h-70 ${
-                              isZoomed ? "transform scale-125" : ""
-                            } 
+	return (
+		<div className="min-h-screen py-0 px-4 text-center">
+			<div className="px-2  text-center xl:grid xl:grid-cols-1 xl:h-[750px]">
+				<div
+					className={
+						!isMobile
+							? "flex xl:flex-row xl:gap-6  xl:items-center lg:mx-20 "
+							: " flex-center"
+					}>
+					<div className="px-10 ">
+						<Carousel
+							orientation="horizontal"
+							className="xl:w-[600px] xl:shadow-lg">
+							<CarouselContent>
+								{Array.from({ length: 3 }).map((_, index) => (
+									<CarouselItem key={index}>
+										<div>
+											<Card>
+												<CardContent className="p-6">
+													<img
+														src={producto.imagen}
+														alt={producto.nombre}
+														onMouseEnter={handleMouseEnter}
+														onMouseLeave={handleMouseLeave}
+														className={`h-70 ${
+															isZoomed ? "transform scale-125" : ""
+														} 
              transition-transform duration-300`}
-                          />
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </div>
-          <div className="xl:flex flex-col  ">
-            <h2 className="text-slate-500 font-medium mt-4 text-lg text-center sm:text-3xl md:text-center xl:text-left xl:text-2xl">
-              {producto.nombre}
-            </h2>
-            <span className="text-xs text-slate-500 px-4 xl:text-left mt-2">
-              Inicio
-            </span>
-            <p className="text-slate-500 font-medium my-4 text-4xl text-left  md:text-center xl:text-left xl:font-semibold">
-              ${producto.precio}
-            </p>
+													/>
+												</CardContent>
+											</Card>
+										</div>
+									</CarouselItem>
+								))}
+							</CarouselContent>
+							<CarouselPrevious />
+							<CarouselNext />
+						</Carousel>
+					</div>
+					<div className="xl:flex flex-col  ">
+						<h2 className="text-slate-500 font-medium mt-4 text-lg text-center sm:text-3xl md:text-center xl:text-left xl:text-2xl">
+							{producto.nombre}
+						</h2>
+						<span className="text-xs text-slate-500 px-4 xl:text-left mt-2">
+							Inicio
+						</span>
+						<p className="text-slate-500 font-medium my-4 text-4xl text-left  md:text-center xl:text-left xl:font-semibold">
+							${producto.precio}
+						</p>
 
-            <div className="border-t-2 border-gray-100 px-4 py-2 xl:w-full xl:border-gray-200 xl:py-0 xl:pt-0">
-              <p className="text-sm md:text-base xl:text-lg">
-                {producto.descripcion}
-              </p>
-              <h2 className="mt-4 mb-2 xl:text-left">Tallas disponibles:</h2>
-              <div className="xl:flex justify-start">
-                <Combobox />
-              </div>
-            </div>
-            <div className="grid grid-cols-2 place-items-center gap-2 mt-6 border-t-2 border-gray-100 py-4 w-full xl:grid-cols-1 xl:place-items-start xl:text-left xl:mb-0 xl:border-gray-200 ">
-              <label>CANTIDAD:</label>
-              <input
-                type="text"
-                className="border-gray-200 border-2 focus:outline-none w-14 h-10 text-center xl:w-20 xl:mb-6"
-              />
-              <label>Elige un color:</label>
-              <div className="flex flex-row flex-nowrap my-0">
-                {colors.map((color) => (
-                  <div
-                    key={color.value}
-                    className={`w-8 h-6 bg-blue-500 rounded-full mx-1 my-2 cursor-pointer border ${
-                      selectedColor === color.value ? "border-sky-500" : ""
-                    }`}
-                    style={{ backgroundColor: color.value }}
-                    onClick={() => handleColorChange(color.value)}
-                  ></div>
-                ))}
-              </div>
-            </div>
-            <Button variant="detail" className="my-2 w-full xl:mt-0 ">
-              AGREGAR AL CARRITO
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<div className="border-t-2 border-gray-100 px-4 py-2 xl:w-full xl:border-gray-200 xl:py-0 xl:pt-0">
+							<p className="text-sm md:text-base xl:text-lg">
+								{producto.descripcion}
+							</p>
+							<h2 className="mt-4 mb-2 xl:text-left">Tallas disponibles:</h2>
+							<div className="xl:flex justify-start">
+								<Combobox />
+							</div>
+						</div>
+						<div className="grid grid-cols-2 place-items-center gap-2 mt-6 border-t-2 border-gray-100 py-4 w-full xl:grid-cols-1 xl:place-items-start xl:text-left xl:mb-0 xl:border-gray-200 ">
+							<label>CANTIDAD:</label>
+							<input
+								type="text"
+								className="border-gray-200 border-2 focus:outline-none w-14 h-10 text-center xl:w-20 xl:mb-6"
+							/>
+							<label>Elige un color:</label>
+							<div className="flex flex-row flex-nowrap my-0">
+								{colors.map((color) => (
+									<div
+										key={color.value}
+										className={`w-8 h-6 bg-blue-500 rounded-full mx-1 my-2 cursor-pointer border ${
+											selectedColor === color.value ? "border-sky-500" : ""
+										}`}
+										style={{ backgroundColor: color.value }}
+										onClick={() => handleColorChange(color.value)}></div>
+								))}
+							</div>
+						</div>
+						<Button variant="detail" className="my-2 w-full xl:mt-0 ">
+							AGREGAR AL CARRITO
+						</Button>
+					</div>
+				</div>
+			</div>
+			<ServicesDetail />
+		</div>
+	);
 };
 
 export default ProductDetail;
