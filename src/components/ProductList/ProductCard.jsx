589c4cee6811fd9a54ff02f23890d94c6c6b4343
Ares <thiagodev.martins@gmail.com>
2024-02-20 04:24:46 -0300

    fixedPrice

--- src/components/ProductList/ProductCard.jsx
+++ src/components/ProductList/ProductCard.jsx
@@ -1,17 +1,28 @@
 const ProductCard = ({ id, name, imageAlt, imageSrc, price }) => {
-  return (
-    <a key={id} className="group">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img
-          src={imageSrc}
-          alt={imageAlt}
-          className="h-80 w-full object-cover object-center group-hover:opacity-75"
-        />
-      </div>
-      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
-    </a>
-  );
+	const priceArray = price.toString().split("");
+
+	if (priceArray.length > 3) {
+		let aux = priceArray.pop();
+		let aux2 = priceArray.pop();
+		let aux3 = priceArray.pop();
+		priceArray.push(".", aux3, aux2, aux);
+	}
+
+	let fixedPrice = priceArray.join("");
+
+	return (
+		<a key={id} className="group">
+			<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+				<img
+					src={imageSrc}
+					alt={imageAlt}
+					className="h-80 w-full object-cover object-center group-hover:opacity-75"
+				/>
+			</div>
+			<h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+			<p className="mt-1 text-lg font-medium text-gray-900">${fixedPrice}</p>
+		</a>
+	);
 };
 
 export default ProductCard;
