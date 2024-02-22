cda3bbb4e0cdfc58bfe0489b911294ef99da8f3f
Ares <thiagodev.martins@gmail.com>
2024-02-22 17:42:47 -0300

    FireBase UserData

--- src/components/Navbar/NavbarDesktop.jsx
+++ src/components/Navbar/NavbarDesktop.jsx
@@ -2,78 +2,107 @@ import { useState } from "react";
 import Login from "../Auth/Login";
 import SearchBar from "../SearchBar/SearchBar";
 import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import UserModal from "../User/UserModal";
 
 const NavbarDesktop = () => {
-  const { pathname } = useLocation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
+	const auth = useAuth();
+	const { displayName } = auth.user;
+	const userName = displayName?.split(" ")[0];
+	const { pathname } = useLocation();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [userModalOpen, setUserModalOpen] = useState(false);
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+		setUserModalOpen(false);
+	};
 
-  return (
-    <nav className="hidden h-20 max-w-screen bg-white px-10 xl:px-40 md:flex items-center gap-2 shadow-md shadow-gray-300 fixed z-10 top-0 w-full">
-      <ul className="flex gap-x-5 items-center flex-wrap font-medium md:max-w-[35%]">
-        <Link to="/">
-          <li>Inicio</li>
-        </Link>
-        <li className="flex gap-1 items-center">
-          Productos{" "}
-          <img
-            src="/assets/navbar-icons/arrow-down.svg"
-            alt="Logo de busqueda"
-            className="pt-1"
-          />
-        </li>
-      </ul>
-      <figure className="mx-auto">
-        <Link to="/">
-          <img
-            src="/assets/images/logo-karokids.png"
-            alt="Logo de KaroKids"
-            className="w-[184px] h-[45px]"
-          />
-        </Link>
-      </figure>
-      <ul className="flex gap-2 items-center">
-        {pathname === "/productos" && (
-          <li>
-            <SearchBar />
-          </li>
-        )}
-        <li onClick={handleOpenModal} className="cursor-pointer">
-          <img
-            src="/assets/navbar-icons/user.svg"
-            alt="Logo usuarios"
-            className="w-6 h-6"
-          />
-        </li>
-        <li>
-          <img
-            src="/assets/navbar-icons/cart-blue.svg"
-            alt="Logo del carrito de compras"
-            className="w-6 h-6"
-          />
-        </li>
-        <li>
-          <img
-            src="/assets/navbar-icons/fav-blue.svg"
-            alt="Logo de Favoritos"
-            className="w-6 h-6"
-          />
-        </li>
+	const handleOpenUser = () => {
+		setUserModalOpen(true);
+	};
 
-        <li className="text-sky-500 font-medium pl-4">
-          <a href="/create">+</a>
-        </li>
-      </ul>
+	const handleCloseUser = () => {
+		setUserModalOpen(false);
+	};
 
-      <Login isOpen={isModalOpen} onClose={handleCloseModal} />
-    </nav>
-  );
+	return (
+		<nav className="hidden h-20 max-w-screen bg-white px-10 xl:px-40 md:flex items-center gap-2 shadow-md shadow-gray-300 fixed z-10 top-0 w-full">
+			<ul className="flex gap-x-5 items-center flex-wrap font-medium md:max-w-[35%]">
+				<Link to="/">
+					<li>Inicio</li>
+				</Link>
+				<li className="flex gap-1 items-center">
+					Productos{" "}
+					<img
+						src="/assets/navbar-icons/arrow-down.svg"
+						alt="Logo de busqueda"
+						className="pt-1"
+					/>
+				</li>
+			</ul>
+			<figure className="mx-auto">
+				<Link to="/">
+					<img
+						src="/assets/images/logo-karokids.png"
+						alt="Logo de KaroKids"
+						className="w-[184px] h-[45px]"
+					/>
+				</Link>
+			</figure>
+			<ul className="flex gap-5 items-center">
+				{pathname === "/productos" && (
+					<li>
+						<SearchBar />
+					</li>
+				)}
+				{userName ? (
+					<li
+						onClick={handleOpenUser}
+						className="flex flex-row gap-x-2 items-end cursor-pointer">
+						<img
+							src="/assets/navbar-icons/user.svg"
+							alt="Logo usuarios"
+							className="w-6 h-6"
+						/>
+						<p className="text-sm">Hola, {userName}</p>
+					</li>
+				) : (
+					<li
+						onClick={handleOpenModal}
+						className="flex flex-row gap-x-2 items-end cursor-pointer">
+						<img
+							src="/assets/navbar-icons/user.svg"
+							alt="Logo usuarios"
+							className="w-6 h-6"
+						/>
+					</li>
+				)}
+
+				<li>
+					<img
+						src="/assets/navbar-icons/fav-blue.svg"
+						alt="Logo de Favoritos"
+						className="w-6 h-6"
+					/>
+				</li>
+
+				<li>
+					<img
+						src="/assets/navbar-icons/cart-blue.svg"
+						alt="Logo del carrito de compras"
+						className="w-6 h-6"
+					/>
+				</li>
+			</ul>
+
+			<Login isOpen={isModalOpen} onClose={handleCloseModal} />
+			<UserModal isOpen={userModalOpen} onClose={handleCloseUser} />
+		</nav>
+	);
 };
 
 export default NavbarDesktop;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,63 +1,108 @@
 import { auth } from "../firebase/firebase.config.js";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithRedirect,
+	signOut,
+	onAuthStateChanged,
+	updateProfile,
 } from "firebase/auth";
 
 export const authContext = createContext();
 
 export const useAuth = () => {
-  const context = useContext(authContext);
-  if (!context) {
-    console.log("error creating auth context");
-  }
-  return context;
+	const context = useContext(authContext);
+	if (!context) {
+		console.log("error creating auth context");
+	}
+	return context;
 };
 
 export function AuthProvider({ children }) {
-	const register = async (email, password) => {
+	const [user, setUser] = useState("");
+	useEffect(() => {
+		const suscribed = onAuthStateChanged(auth, (currentUser) => {
+			if (!currentUser) {
+				setUser("");
+			} else {
+				setUser(currentUser);
+			}
+		});
+		return () => suscribed();
+	}, []);
+
+	const register = async (email, password, displayName) => {
 		try {
 			const response = await createUserWithEmailAndPassword(
 				auth,
 				email,
 				password
 			);
+			await updateDisplayName(response.user, displayName);
+			alert("Te has registrado correctamente.");
 		} catch (error) {
 			alert("Ya existe un usuario asociado a este correo");
+		}
+	};
+
+	const updateDisplayName = async (user, displayName) => {
+		try {
+			await updateProfile(user, { displayName });
+		} catch (error) {
+			alert("No se pudo actualizar el display name");
 		}
 	};
 
 	const login = async (email, password) => {
 		try {
 			const response = await signInWithEmailAndPassword(auth, email, password);
+			alert("Has ingresado exitosamente.");
 		} catch (error) {
 			alert("No existe ningun usuario asociado a este correo");
 		}
 	};
 
-  const loginWithGoogle = async () => {
-    try {
-      const responseGoogle = new GoogleAuthProvider();
-      return await signInWithPopup(auth, responseGoogle);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const loginWithGoogle = async () => {
+		try {
+			const responseGoogle = new GoogleAuthProvider();
+			return await signInWithRedirect(auth, responseGoogle);
+			alert("Has ingresado exitosamente.");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const logout = async () => {
-    try {
-      const responseOut = signOut();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <authContext.Provider value={{ register, login, loginWithGoogle, logout }}>
-      {children}
-    </authContext.Provider>
-  );
+	const registerWithGoogle = async () => {
+		try {
+			const responseGoogle = new GoogleAuthProvider();
+			return await signInWithRedirect(auth, responseGoogle);
+			alert("Te has registrado correctamente.");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const logout = async () => {
+		try {
+			const response = await signOut(auth);
+			alert("Sesion finalizada con éxito");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<authContext.Provider
+			value={{
+				register,
+				login,
+				loginWithGoogle,
+				registerWithGoogle,
+				logout,
+				user,
+			}}>
+			{children}
+		</authContext.Provider>
+	);
 }
