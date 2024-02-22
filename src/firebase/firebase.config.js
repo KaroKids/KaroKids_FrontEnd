aea9140b081c0c550c8efd7bb3bcdd8ec03b0ea2
LGKaufmann <lautykaufmann76@gmail.com>
2024-02-22 13:47:49 -0300

    login y modal

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,27 +1,19 @@
 // Import the functions you need from the SDKs you need
-require("dotenv").config();
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-const {
-	apiKey,
-	authDomain,
-	projectId,
-	storageBucket,
-	messagingSenderId,
-	appId,
-	measurementId,
-} = process.env;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
-	apiKey: apiKey,
-	authDomain: authDomain,
-	projectId: projectId,
-	storageBucket: storageBucket,
-	messagingSenderId: messagingSenderId,
-	appId: appId,
-	measurementId: measurementId,
+  apiKey: "AIzaSyAbv1kvAXUiJfomGtmcAJqp7DgU6wQohrw",
+  authDomain: "karokids-2cb6a.firebaseapp.com",
+  projectId: "karokids-2cb6a",
+  storageBucket: "karokids-2cb6a.appspot.com",
+  messagingSenderId: "309517758272",
+  appId: "1:309517758272:web:89c6f76e1d61ef85c114a5",
+  measurementId: "G-DH8K74YVBB",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
