f93211aec63843f1c2059e9c1c46daf3b5442066
EFDillmann <dillmanncode1999@gmail.com>
2024-02-27 01:38:04 -0300

    conditional rendering productslist

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -113,7 +113,7 @@ export default function ProductList() {
 
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {productos &&
-              productos.productos.map((product, i) => (
+              productos?.productos?.map((product, i) => (
                 <Link key={i} to={`/producto/${product.producto_id}`}>
                   <ProductCard
                     id={product.producto_id}
