eab64c8c00085033a463927aab537ea1d64d3551
LGKaufmann <lautykaufmann76@gmail.com>
2024-03-02 22:42:49 -0300

    fixed admin y carrousel destacados

--- src/components/Navbar/NavbarDesktop.jsx
+++ src/components/Navbar/NavbarDesktop.jsx
@@ -7,111 +7,117 @@ import UserModal from "../User/UserModal";
 import { useDispatch, useSelector } from "react-redux";
 
 const NavbarDesktop = () => {
-	const auth = useAuth();
-	const { displayName } = auth.user;
-	const userName = displayName?.split(" ")[0];
-	const { pathname } = useLocation();
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [userModalOpen, setUserModalOpen] = useState(false);
-	const handleOpenModal = () => {
-		setIsModalOpen(true);
-	};
+  const auth = useAuth();
+  const { displayName } = auth.user;
+  const userName = displayName?.split(" ")[0];
+  const { pathname } = useLocation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userModalOpen, setUserModalOpen] = useState(false);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-	const handleCloseModal = () => {
-		setIsModalOpen(false);
-		setUserModalOpen(false);
-	};
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setUserModalOpen(false);
+  };
 
-	const handleOpenUser = () => {
-		setUserModalOpen(true);
-	};
+  const handleOpenUser = () => {
+    setUserModalOpen(true);
+  };
 
-	const handleCloseUser = () => {
-		setUserModalOpen(false);
-	};
-
+  const handleCloseUser = () => {
+    setUserModalOpen(false);
+  };
 
   const user = useSelector((state) => state.users.user);
 
-	return (
-		<nav className="hidden h-20 max-w-screen bg-white px-10 xl:px-40 md:flex items-center gap-2 shadow-md shadow-gray-300 fixed z-10 top-0 w-full">
-			<ul className="flex gap-x-5 items-center flex-wrap font-medium md:max-w-[35%]">
-				<Link to="/">
-					<li>Inicio</li>
-				</Link>
-				{user?.roles === "admin" && (
-					<Link to="/admin">
-						<li>Administración</li>
-					</Link>
-				)}
-				<Link to="/productos">
-					<li>Productos</li>
-				</Link>
-			</ul>
-			<figure className="mx-auto">
-				<Link to="/">
-					<img
-						src="/assets/images/logo-karokids.png"
-						alt="Logo de KaroKids"
-						className="w-[184px] h-[45px]"
-					/>
-				</Link>
-			</figure>
-			<ul className="flex gap-5 items-center">
-				{pathname === "/productos" && (
-					<li>
-						<SearchBar />
-					</li>
-				)}
-				{userName ? (
-					<li
-						onClick={handleOpenUser}
-						className="flex flex-row gap-x-2 items-end cursor-pointer">
-						<img
-							src="/assets/navbar-icons/user.svg"
-							alt="Logo usuarios"
-							className="w-6 h-6"
-						/>
-						<p className="text-sm">Hola, {userName}</p>
-					</li>
-				) : (
-					<li
-						onClick={handleOpenModal}
-						className="flex flex-row gap-x-2 items-end cursor-pointer">
-						<img
-							src="/assets/navbar-icons/user.svg"
-							alt="Logo usuarios"
-							className="w-6 h-6"
-						/>
-					</li>
-				)}
+  return (
+    <nav className="hidden h-20 max-w-screen bg-white px-10 xl:px-40 md:flex items-center gap-2 shadow-md shadow-gray-300 fixed z-10 top-0 w-full">
+      <ul className="flex gap-x-5 items-center flex-wrap font-medium md:max-w-[35%]">
+        <Link to="/">
+          <li>Inicio</li>
+        </Link>
+        {user?.roles === "admin" && (
+          <Link to="/admin">
+            <li>Administración</li>
+          </Link>
+        )}
+        <Link to="/productos">
+          <li>Productos</li>
+        </Link>
+      </ul>
+      <figure className="mx-auto">
+        <Link to="/">
+          <img
+            src="/assets/images/logo-karokids.png"
+            alt="Logo de KaroKids"
+            className="w-[184px] h-[45px]"
+          />
+        </Link>
+      </figure>
+      <ul className="flex gap-5 items-center">
+        {pathname === "/productos" && (
+          <li>
+            <SearchBar />
+          </li>
+        )}
+        {userName ? (
+          <li
+            onClick={handleOpenUser}
+            className="flex flex-row gap-x-2 items-end cursor-pointer"
+          >
+            <img
+              src="/assets/navbar-icons/user.svg"
+              alt="Logo usuarios"
+              className="w-6 h-6"
+            />
+            <p className="text-sm">Hola, {userName}</p>
+          </li>
+        ) : (
+          <li
+            onClick={handleOpenModal}
+            className="flex flex-row gap-x-2 items-end cursor-pointer"
+          >
+            <img
+              src="/assets/navbar-icons/user.svg"
+              alt="Logo usuarios"
+              className="w-6 h-6"
+            />
+          </li>
+        )}
 
-				<li className="cursor-pointer">
-					<img
-						src="/assets/navbar-icons/fav-blue.svg"
-						alt="Logo de Favoritos"
-						className="w-6 h-6"
-					/>
-				</li>
-				<Link to="/carrito">
-					<li className="cursor-pointer">
-						<img
-							src="/assets/navbar-icons/cart-blue.svg"
-							alt="Logo del carrito de compras"
-							className="w-6 h-6"
-						/>
-					</li>
-				</Link>
-			</ul>
+        {auth.user ? (
+          <li className="cursor-pointer">
+            <img
+              src="/assets/navbar-icons/fav-blue.svg"
+              alt="Logo de Favoritos"
+              className="w-6 h-6"
+            />
+          </li>
+        ) : (
+          ""
+        )}
 
-			<Login
-				isOpen={isModalOpen}
-				onClose={handleCloseModal}
-				className={`transition-opacity duration-300 ease-in-out ${isModalOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-			/>
-			<UserModal isOpen={userModalOpen} onClose={handleCloseUser} />
-		</nav>
-	);
+        <Link to="/carrito">
+          <li className="cursor-pointer">
+            <img
+              src="/assets/navbar-icons/cart-blue.svg"
+              alt="Logo del carrito de compras"
+              className="w-6 h-6"
+            />
+          </li>
+        </Link>
+      </ul>
+
+      <Login
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        className={`transition-opacity duration-300 ease-in-out ${isModalOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+      />
+      <UserModal isOpen={userModalOpen} onClose={handleCloseUser} />
+    </nav>
+  );
 };
 
 export default NavbarDesktop;
