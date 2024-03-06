2cf6c3e2dd066e29e6aa7027c57e55338745ad22
EFDillmann <dillmanncode1999@gmail.com>
2024-03-05 21:51:43 -0300

    eliminacion de logs

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -39,11 +39,9 @@ export default function Login({ isOpen, onClose, className }) {
 
   const detectedLS = async (mail) => {
     try {
-      console.log(mail);
       const { payload } = await dispatch(getUserByEmail(mail));
       const storedProducts = JSON.parse(localStorage.getItem("cart"));
       const usuario_id = payload.usuario_id;
-      console.log(usuario_id);
       if (storedProducts && storedProducts.length > 0) {
         await dispatch(
           addProductInDB({
